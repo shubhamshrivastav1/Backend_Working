403f211950a72b3d6314541beb62762a05afa82f
ssk06112000 <ssk06112000@gmail.com>
2026-08-12 00:45:29 +0530

    some changes

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -33,7 +33,7 @@ const userSchema = new Schema(
       coverImage: {
         type: String,   //cloudinary url
       },
-      watchHistroy: [  
+      watchHistory: [  
       {
         type: Schema.Types.ObjectId,
         ref: "Video"
